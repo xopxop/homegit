6aef86d6e462ab983f606866792ec84d0d83e116
xopxop <xopxop.1403@gmail.com>
2019-10-28 16:43:17 +0200

    Move ft_lstdel & ft_lstdelone out and add ft_lstadd

--- libft/dthan/libft.h
+++ libft/dthan/libft.h
@@ -6,7 +6,7 @@
 /*   By: dthan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 11:32:53 by dthan             #+#    #+#             */
-/*   Updated: 2019/10/28 15:29:31 by dthan            ###   ########.fr       */
+/*   Updated: 2019/10/28 16:39:44 by dthan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,8 @@ void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void const *content, size_t content_size);
 //void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t);
+//void	ft_lstdel(t_list **alst, void (*del)(void *, size_t);
+void	ft_lstadd(t_list **alst, t_list *new);
 int					ft_isspace(int c);
 int					ft_countwords(char const *str, char c);
 
