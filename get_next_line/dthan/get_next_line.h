5a98b40bc84a895733ce082b845d06f4d5ee19d3
xopxop <xopxop.1403@gmail.com>
2019-11-08 04:34:08 +0200

    Big BUFF_SIZE verified

--- get_next_line/dthan/get_next_line.h
+++ get_next_line/dthan/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: dthan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 01:39:52 by dthan             #+#    #+#             */
-/*   Updated: 2019/11/07 01:10:46 by dthan            ###   ########.fr       */
+/*   Updated: 2019/11/08 04:32:10 by dthan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 8
+# define BUFF_SIZE 1000
 # define FD_MAX 256
 # include <stdlib.h>
 # include <stdio.h>
