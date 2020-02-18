931ab6d967eef73ae6bdc90dd44786f3788ebb7e
xopxop <xopxop.1403@gmail.com>
2020-02-18 17:39:15 +0200

    Level 0 Ok

--- exam_report/examshell/level0/aff_last_param/aff_last_param.c
+++ exam_report/examshell/level0/aff_last_param/aff_last_param.c
@@ -6,8 +6,8 @@ int main(int argc, char **argv)
 
     i = 0;
     if (argc > 1)
-        while (argv[1][i] != '\0')
-            write(1, &argv[1][i++], 1);
-    write(1, "\n", 1);
+        while (argv[argc - 1][i] != '\0')
+            write(1, &argv[argc - 1][i++], 1);
+    write (1, "\n", 1);
     return (0);
 }
