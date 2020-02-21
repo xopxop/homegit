969bf7cab16d674751445912e06316cb6f798338
xopxop <xopxop.1403@gmail.com>
2020-02-21 08:45:27 +0200

    Started ft_ls project

---
+++ ft_ls/includes/ft_ls.h
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dthan <dthan@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/21 04:58:25 by dthan             #+#    #+#             */
+/*   Updated: 2020/02/21 04:58:26 by dthan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+/*
+** opendir <sys/types.h><dirent.h>
+** ==> open a directory
+** readdir <dirent.h>
+** ==> read a direcotry.
+** closedir <sys/types.h><dirent.h>
+** ==> close a directory.
+** stat _none_
+** ==> Display file or file system status.
+** lstat <sys/types.h><sys/stat.h><unistd.h>
+** ==> get file status.
+** getpwuid <sys/types.h><pwd.h>
+** ==> get password file entry.
+** getgrgid <sys/types.h><grp.h>
+** ==> get group file entry.
+** time -none-
+** ==> run programs and summarize system resource usage.
+** ctime <grp.h>
+** ==> transform date and time to broken-down time or ASCII
+** readlink -none-
+** ==> print resolved symbolic links or canonical file names.
+** perror <stdio.h>
+** ==> print a system error message.
+** strerror <string.h>
+** ==> return string describing error number.
+*/
+
+# include <sys/types.h>
+# include <dirent.h>
+# include <pwd.h>
+# include <grp.h>
+# include <stdio.h>
+# include <string.h>
+
+#endif
