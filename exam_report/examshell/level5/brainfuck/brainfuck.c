4342f3f287c31cdadb86a91a6fda92292ef92639
xopxop <xopxop.1403@gmail.com>
2020-02-26 07:23:54 +0200

    Day01: brainfuck

--- exam_report/examshell/level5/brainfuck/brainfuck.c
+++ exam_report/examshell/level5/brainfuck/brainfuck.c
@@ -1,72 +1,18 @@
-/*
-#include <unistd.h>
-
-int	goto_match(char *str, int i, int step)
-{
-	int bracket_depth = step;
-
-	while (i >= 0 && i <= 4096)
-	{
-		i += step;
-		if (str[i] == '[')
-			++bracket_depth;
-		else if (str[i] == ']')
-			--bracket_depth;
-		if (bracket_depth == 0)
-			return (i);
-	}
-	return (0);
-}
-
-void	brainfuck(char *str)
-{
-	char arr[2049] = { [0 ... 2048] = 0 };
-	int ptr = 0;
-	int i = 0;
-
-	while (str[i])
-	{
-		if (str[i] == '>')
-			++ptr;
-		else if (str[i] == '<')
-			--ptr;
-		else if (str[i] == '+')
-			arr[ptr]++;
-		else if (str[i] == '-')
-			arr[ptr]--;
-		else if (str[i] == '.')
-			write(1, arr + ptr, 1);
-		else if (str[i] == '[' && arr[ptr] == 0)
-			i = goto_match(str, i, 1);
-		else if (str[i] == ']' && arr[ptr] != 0)
-			i = goto_match(str, i, -1);
-		++i;
-	}
-}
-
-int	main(int ac, char **av)
-{
-	if (ac >= 2)
-		brainfuck(av[1]);
-	else
-		write(1, "\n", 1);
-	return (0);
-}
-*/
-
 #include <unistd.h>
 
 int goto_match(char *str, int i, int step)
 {
-	int breck = step;
-	while(i >= 0 && i <= 4096)
+	int bracket_nbr;
+
+	bracket_nbr = step;
+	while (i >= 0 && i <= 4096)
 	{
 		i += step;
-		if (str[i] == '[')
-			++breck;
-		else if (str[i] == ']')
-			--breck;
-		if (breck == 0)
+		if (str[i] == ']')
+			bracket_nbr--;
+		else if (str[i] == '[')
+			bracket_nbr++;
+		if (bracket_nbr == 0)
 			return (i);
 	}
 	return (0);
@@ -74,37 +20,35 @@ int goto_match(char *str, int i, int step)
 
 void brainfuck(char *str)
 {
-	char array[2049] = { [0 ... 2048] = 0};
-	char *ptr;
+	char array[2049] = {[0 ... 2048] = 0};
+	int ptr;
 	int i = 0;
 
-	ptr = &array[0];
-	while (str[i])
+	ptr = 0;
+	while (str[i] != '\0')
 	{
 		if (str[i] == '>')
 			ptr++;
 		else if (str[i] == '<')
 			ptr--;
 		else if (str[i] == '+')
-			(*ptr)++;
+			array[ptr]++;
 		else if (str[i] == '-')
-			(*ptr)--;
+			array[ptr]--;
 		else if (str[i] == '.')
-			write(1, &array[*ptr], 1);
-		else if (str[i] == '[' && *ptr == 0)
-			i = goto_match(str, i , 1);
-		else if (str[i] == ']' && *ptr != 0)
-			i = goto_match(str, i , -1);
-		++i;
+			write(1, &array[ptr], 1);
+		else if (str[i] == '[' && array[ptr] == 0)
+			i = goto_match(str, i, 1);
+		else if (str[i] == ']' && array[ptr] != 0)
+			i = goto_match(str, i, -1);
+		i++;
 	}
 }
 
 int main(int ac, char **av)
 {
 	if (ac >= 2)
-	{
 		brainfuck(av[1]);
-	}
 	else
 		write(1, "\n", 1);
 	return (0);
