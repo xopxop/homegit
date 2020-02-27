31b30e387c0e1525a69dc1e4b4c149638ea43dd8
xopxop <xopxop.1403@gmail.com>
2020-02-27 11:11:26 +0200

    For testing

--- ft_ls/srcs/ft_ls.c
+++ ft_ls/srcs/ft_ls.c
@@ -11,37 +11,100 @@
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
+# define ISFLAG 1
+# define IS_NOT_FLAG 0
 
-void ft_ls(char **input)
+int get_options(char *input, int *options)
 {
-    int options;
-    // First we pare the input, i means checking the flag
-    options = parse_input(input);
-    // 
-    print_input();
-}
-
-int main(int argc, char **argv)
-{
-    void ft_ls(argv);    
-    return (0);
-}
-
-/*
-int main(int argc, char **argv)
-{
-    DIR *dir;
-    struct dirent *sd;
-
-    dir = opendir(".");
-    if (dir == NULL)
+    input++;
+    while(*input)
     {
-        ft_printf("Error! Unable to open directory.\n");
-        exit(1);
+        if (*input++ == 'l')
+            *options |= LONG_LIST_FORMAT; 
+        else if (*input++ == 'R')
+            *options |= LIST_SUBDIR_RECUSIVELY;
+        else if (*input++ == 'a')
+            *options |= LIST_HIDDEN;
+        else if (*input++ == 'r')
+            *options |= REVERSE_ORDER;
+        else if (*input++ == 't')
+            *options |= SORT_BY_NEWEST_FIRST;
+        else
+            return(SERIOUS_TROUBLE);
     }
-    while ((sd=readdir(dir)) != NULL)
-        ft_printf(">> %s\n", sd->d_name);
-    closedir(dir);
+    return (OK);
+}
+
+int get_dir(char *input, t_dinfo **ldir)
+{
+    DIR *current_dir;
+    t_dinfo *new;
+
+    if (!(new = (t_dinfo*)ft_memalloc(sizeof(t_dinfo))))
+        return (MINOR_PROBLEMS);
+    new->dir_name = ft_strdup(input);
+    current_dir = opendir(new->dir_name);
+    if (current_dir == NULL)
+        return (MINOR_PROBLEMS);
+    closedir(current_dir);
+    new->next = NULL;
+    if (!*ldir)
+        *ldir = new;
+    else
+        (*ldir)->next = new;
+    return (OK);
+}
+
+void output(/*int options, */t_dinfo *dir)
+{
+    DIR *current_dir;
+    struct dirent *entry;
+    struct stat filestat;
+
+    while(dir != NULL)
+    {
+        current_dir = opendir(dir->dir_name);
+        while ((entry = readdir(current_dir)))
+        {
+            //Test
+            stat(entry->d_name, &filestat);
+            if (S_ISDIR(filestat.st_mode))
+                ft_printf("%s: %s\n", "DIR", entry->d_name);
+            else
+                ft_printf("%s: %s\n", "FILE", entry->d_name);
+        }
+        closedir(current_dir);
+        dir = dir->next;
+    }
+}
+
+int ft_ls(char **input)
+{
+    int arg_nbr;
+    int options;
+    int exit_status;
+    t_dinfo *ldir;
+
+    arg_nbr = 1;
+    ldir = NULL;
+    while(input[arg_nbr])
+    {
+        if (input[arg_nbr][0] == '-')
+            exit_status = get_options(input[arg_nbr], &options);
+        else
+            exit_status = get_dir(input[arg_nbr], &ldir);
+        if (exit_status == MINOR_PROBLEMS || exit_status == SERIOUS_TROUBLE)
+            return (exit_status);
+        arg_nbr++;
+    }
+    output(/*options, */ ldir);
+    return (OK);
+}
+
+int main(int ac, char **av)
+{
+    // test ac
+    if (ac > -3)
+        return (ft_ls(av));
     return (0);
 }
-*/
