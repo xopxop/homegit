8b7208da7c0b7b9d6a39e800f2a9d05dd82d0d35
dthan <xopxop.1403@gmail.com>
2020-06-22 14:08:14 +0300

    Day06: ex00, ex01, ex02, unfinished

---
+++ Piscine_PHP/d06/ex02/Vector.class.php
@@ -0,0 +1,90 @@
+<?PHP
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Vector.class.php                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dthan <dthan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/22 06:24:32 by dthan             #+#    #+#             */
+/*   Updated: 2020/06/22 06:24:32 by dthan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+require_once 'Vertex.class.php';
+
+Class Vector {
+	private $_x;
+	private $_y;
+	private $_z;
+	private $_w;
+	static $verbose = false;
+
+	public function __construct( $array ) {
+		// if ( array_key_exists( 'orig', $array ) && $array['orig'] instanceof Vertex ) {
+		// 	$this->_x = $array['dest']->_x - $array['orig']->_x;
+		// 	$this->_y = $array['dest']->_y - $array['orig']->_y;
+		// 	$this->_z = $array['dest']->_z - $array['orig']->_z;
+		// 	// What is $this->_w here?
+		// }
+		// else {
+		// 	$orig = new Vertex( array( 'x'=0, 'y'=0, 'z'=0, 'w'=1 ) );
+		// 	$this->_x = $array['dest']->_x - $org->_x;
+		// 	$this->_y = $array['dest']->_y - $org->_y;
+		// 	$this->_z = $array['dest']->_z - $org->_z;
+		// 	// $this->_w
+		// }
+		if ( !array_key_exists( 'orig', $array ) && $array['orig'] instanceof Vertex )
+			$orig = $array['orig'];
+		else
+			$orig = new Vertex( array( 'x'=>0, 'y'=>0, 'z'=>0, 'w'=>1 ) );
+		$this->_x = $array['dest']->_x - $org->_x;
+		$this->_y = $array['dest']->_y - $org->_y;
+		$this->_z = $array['dest']->_z - $org->_z;
+		// $this->_w
+		if (self::$verbose)
+			echo $this->__toString() . " constructed" . PHP_EOL;
+	}
+
+	public function __destruct() {
+		if (self::$verbose)
+			echo $this->__toString() . " destructed" . PHP_EOL;
+		return ;
+	}
+
+	public function __toString() {
+		return sprintf( "Vector( x:%0.2f, y:%0.2f, z:%0.2f, w:%0.2f )", $this->_x, $this->_y, $this->_z , $this->_w );
+	}
+
+	public function doc() {
+		if ( $str = file_get_contents( 'Vector.doc.txt' ) )
+			echo $str;
+		else
+			echo "Error: .doc file doesn't exist." . PHP_EOL;
+	}
+
+	public function magnitude() {
+		return (float)sqrt(pow($this->_x, 2) + pow($this->_y, 2) + pow($this->_z, 2));
+	}
+
+	public function normalize() {
+		$magnitude = $this->magnitude();
+		if ($magnitude == 1)
+			return clone $this;
+		return new Vector ( array( 'x'=>$this->_x/$magnitude, 'y'=>$this->_y/$magnitude, 'z'=>$this->_z/$magnitude ) );
+	}
+
+	public function add( Vector $rhs ) {
+		return new Vector ( array( 'x'=>$this->_x+$rhs->_x, 'y'=>$this->_y+$rhs->_y, 'z'=>$this->_z+$rhs->_z ) );
+	}
+
+	public function sub( Vector $rhs ) {
+		return new Vector ( array( 'x'=>$this->_x-$rhs->_x, 'y'=>$this->_y-$rhs->_y, 'z'=>$this->_z-$rhs->_z ) );
+	}
+
+	public function opposite() {
+		return new Vector ( array( 'x'=>$this->_x*-1, 'y'=>$this->_y*-1, 'z'=>$this->_z*-1 ) );
+	}
+}
+
+?>
---
+++ Piscine_PHP/d06/ex02/Vertex.class.php
@@ -0,0 +1,59 @@
+<?PHP
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Vertex.class.php                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dthan <dthan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/22 04:57:25 by dthan             #+#    #+#             */
+/*   Updated: 2020/06/22 04:57:25 by dthan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+require_once 'Color.class.php';
+
+Class Vertex {
+	private $_x;
+	private $_y;
+	private $_z;
+	private $_w = 1.0;
+	private $_color;
+	static $verbose = false;
+
+	public function __construct( $array ) {
+		$this->_x = $array['x'];
+		$this->_y = $array['y'];
+		$this->_z = $array['z'];
+		if ( array_key_exists( 'w', $array ) )
+			$this->_w = $array['w'];
+		if ( array_key_exists( 'color', $array ) && $array['color'] instanceof Color )
+			$this->_color = $array['color'];
+		else
+			$this->_color = new Color( array( 'red'=>255, 'green'=>255, 'blue'=>255 ) );
+		if (self::$verbose)
+			echo $this->__toString() . " constructed" . PHP_EOL;
+	}
+
+	public function __destruct() {
+		if (self::$verbose)
+			echo $this->__toString() . " destructed" . PHP_EOL;
+		return ;
+	}
+	public function __toString() {
+		$str = sprintf( "Vertex( x: %0.2f, y: %0.2f, z:%0.2f, w:%0.2f", $this->_x, $this->_y, $this->_z , $this->_w );
+		if (self::$verbose == true)
+			$str .= ", " . $this->_color->__toString();
+		$str .= " )";
+		return $str;
+	}
+
+	public function doc() {
+		if ( $str = file_get_contents( 'Vertex.doc.txt' ) )
+			echo $str;
+		else
+			echo "Error: .doc file doesn't exist." . PHP_EOL;
+	}
+}
+
+?>
