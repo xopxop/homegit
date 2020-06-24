12a394b5df84b6126766c09bd56289773800cab9
dthan <xopxop.1403@gmail.com>
2020-06-24 17:01:33 +0300

    fixed rostring

--- Piscine_PHP/d01/ex07/rostring.php
+++ Piscine_PHP/d01/ex07/rostring.php
@@ -7,9 +7,7 @@ function ft_split($string) {
 
 if ($argc > 1) {
 	$my_array = ft_split($argv[1]);
-	print_r($my_array);
 	$size = count($my_array);
-	echo $my_array[0] . PHP_EOL;
 	if ($size > 1)
 		for ($i = 1; $i < $size; $i++)
 			echo "$my_array[$i] ";
