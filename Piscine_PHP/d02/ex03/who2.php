1a4b2a8d5946c6876246447d74e6f532b7007752
dthan <xopxop.1403@gmail.com>
2020-06-25 18:52:46 +0300

    test

--- Piscine_PHP/d02/ex03/who2.php
+++ Piscine_PHP/d02/ex03/who2.php
@@ -10,7 +10,7 @@
         if ($i >= 2 * 628 && $tab['type'] == 7)
         {
             $time = strftime("%b %e %R", $tab['time']);
-            array_push($who , str_pad($tab['login'], 8, " "). str_ped($tab['tty'], 8, " ") .$time);
+            array_push($who , str_pad($tab['login'], 8, " "). str_pad($tab['tty'], 8, " ") .$time);
         }
     $i = $i + 628;
     }
