fab9f80890fe071a325f43e1373e62701bca6d78
dthan <xopxop.1403@gmail.com>
2020-06-26 05:17:18 +0300

    test

--- Piscine_PHP/d03/ex06/members_only.php
+++ Piscine_PHP/d03/ex06/members_only.php
@@ -7,6 +7,7 @@
 		echo "'>\n</body></html>\n";
 	}
 	else {
+		header_remove('Server');
 		header("WWW-Authenticate: Basic realm=''Member area''");
 		header('HTTP/1.0 401 Unauthorized');
 		echo "<html><body>That area is accessible for members only</body></html>     \n";
