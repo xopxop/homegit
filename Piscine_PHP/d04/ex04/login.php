7def9daed1c5afb91d062b1e425092d495dd2ad9
xopxop <xopxop.1403@gmail.com>
2020-11-14 16:39:49 +0200

    ok

--- Piscine_PHP/d04/ex04/login.php
+++ Piscine_PHP/d04/ex04/login.php
@@ -5,10 +5,12 @@ include 'auth.php';
 if ($_POST['login'] && $_POST['passwd']) {
 	if (auth($_POST['login'], $_POST['passwd']) == true) {
 		$_SESSION['loggued_on_user'] = $_POST['login'];
-		echo "<html><body>" . PHP_EOL;
-		echo "	<iframe name=\"chat\" src=\"chat.php\" width=\"100%\" height=\"550px\"></iframe>" . PHP_EOL;
-		echo "	<iframe name=\"speak\" src=\"speak.php\" width=\"100%\" height=\"50px\"></iframe>" . PHP_EOL;
-		echo "</div></body></html>" . PHP_EOL;
+		echo "<html>" . PHP_EOL;
+		echo "	<body>" . PHP_EOL;
+		echo "		<iframe name=\"chat\" src=\"chat.php\" width=\"100%\" height=\"550px\"></iframe>" . PHP_EOL;
+		echo "		<iframe name=\"speak\" src=\"speak.php\" width=\"100%\" height=\"50px\"></iframe>" . PHP_EOL;
+		echo "	</body>" . PHP_EOL;
+		echo "</html>" . PHP_EOL;
 	} else {
 		echo "ERROR" . PHP_EOL;
 	}
